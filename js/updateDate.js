16c0d52eb78a00139e7242a1fc3283d95b07f63c
anjella1994 <anjella1994@naver.com>
2023-10-29 02:37:59 +0900

    모바일 버전 기본으로 잠시 변경
    높이 오류 수정

--- js/updateDate.js
+++ js/updateDate.js
@@ -1,4 +1,4 @@
-const divHeight = document.querySelector("#table div").clientHeight + 1;
+const divHeight = 0.15 * window.innerWidth;
 
 const years = [2020, 2021, 2022];
 const months = ["june", "july", "august", "september", "october","november", "december", 
