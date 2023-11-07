da17b46690ac9d4b9d2b12e134ebcf179687cd11
anjella1994 <anjella1994@naver.com>
2023-11-07 23:05:16 +0900

    터치 일부 수정

--- js/m-slide-moveSlide.js
+++ js/m-slide-moveSlide.js
@@ -1,3 +1,11 @@
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+
+let touchStartX = 0;
+let touchEndX = 0;
+let touchStartY = 0;
+let touchEndY = 0;
+
 //슬라이드 이동 함수 선언
 function navigate(isRight) {
     if (isRight) {
@@ -13,14 +21,6 @@ function navigate(isRight) {
     window.location.reload();
 };
 
-const leftArrow = document.querySelector(".left");
-const rightArrow = document.querySelector(".right");
-
-let touchStartX = 0;
-let touchEndX = 0;
-let touchStartY = 0;
-let touchEndY = 0;
-
 //클릭 시 왼쪽, 오른쪽 이동
 leftArrow.addEventListener("click", () => navigate(false));
 rightArrow.addEventListener("click", () => navigate(true));
@@ -31,8 +31,8 @@ document.addEventListener("touchstart", (e) => {
     touchStartY = e.touches[0].clientY;
 });
 document.addEventListener("touchend", (e) => {
-    touchEndX = e.touches[0].clientX;
-    touchEndY = e.touches[0].clientY;
+    touchEndX = e.changedTouches[0].clientX;
+    touchEndY = e.changedTouches[0].clientY;
     if (Math.abs(touchStartY - touchEndY < 10)) { //수직 이동이 거의 없을 때만 실행 
         if (touchStartX - touchEndX > 50) {
             navigate(true);
@@ -41,3 +41,52 @@ document.addEventListener("touchend", (e) => {
         }
     }
 });
+
+// function goLeft() {
+//     if(rowNum == 0) {
+//         rowNum = 673;
+//     } else {
+//         rowNum--;
+//     }
+//     localStorage.setItem("clickedDivNum", rowNum * 4);
+//     getDate(rowNum);
+//     updateCurrentDate(currentFullDate);
+//     inputImage(currentFullDate);
+//     inputTitle(rowNum);
+//     window.location.reload();
+// }
+
+// function goRight() {
+//     if(rowNum == 673) {
+//         rowNum = 0;
+//     } else {
+//         rowNum++;
+//     }
+//     localStorage.setItem("clickedDivNum", rowNum * 4);
+//     getDate(rowNum);
+//     updateCurrentDate(currentFullDate);
+//     inputImage(currentFullDate);
+//     inputTitle(rowNum);
+//     window.location.reload();
+// }
+
+// leftArrow.addEventListener("click", goLeft);
+// rightArrow.addEventListener("click", goRight);
+
+
+// document.addEventListener("touchstart", function(e){
+//     touchStartX = e.touches[0].clientX;
+// });
+// document.addEventListener("touchend", function(e){
+//     touchEndX = e.changedTouches[0].clientX;
+//     handleSwipe();
+// });
+
+// function handleSwipe() {
+//     if (touchStartX - touchEndX > 100) {
+//         goRight();
+//     } else if (touchEndX - touchStartX > 100) {
+//         goLeft();
+//     }
+// }
+
