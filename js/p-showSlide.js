d99814974d5ffca96b7f3235b7eec1b4b1082c71
anjella1994 <anjella1994@naver.com>
2023-11-12 04:28:33 +0900

    pc버전 터치 이벤트 추가

--- js/p-showSlide.js
+++ js/p-showSlide.js
@@ -2,17 +2,13 @@ const dateNumBoxes = document.querySelectorAll(".image div");
 document.addEventListener("DOMContentLoaded", function() {
     //마우스 갖다대면 생기는 날짜 박스
     dateNumBoxes.forEach(function(dateBox, index) { 
-        dateBox.addEventListener("click", function(){
+        function goToSlide() {
             //클릭한 div의 인덱스를 localstorage에 저장
             localStorage.setItem("clickedDivNum", index);
             //이미지 슬라이드 페이지로 이동 
             window.location.href = "index-pc-slide.html";
-        });
-        dateBox.addEventListener("dblclick", function(){
-            //클릭한 div의 인덱스를 localstorage에 저장
-            localStorage.setItem("clickedDivNum", index);
-            //이미지 슬라이드 페이지로 이동 
-            window.location.href = "index-pc-slide.html";
-        });
+        }
+        dateBox.addEventListener("click", goToSlide);
+        dateBox.addEventListener("touchstart", goToSlide);    
     });
 });
