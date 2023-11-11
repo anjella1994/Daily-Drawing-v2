9c458f76490cdfacd24b02ddbeeb11fecbc9f795
anjella1994 <anjella1994@naver.com>
2023-11-12 04:23:54 +0900

    pc버전 슬라이드로 이동 시 더블클릭 이벤트 추가

--- js/p-showSlide.js
+++ js/p-showSlide.js
@@ -8,5 +8,11 @@ document.addEventListener("DOMContentLoaded", function() {
             //이미지 슬라이드 페이지로 이동 
             window.location.href = "index-pc-slide.html";
         });
+        dateBox.addEventListener("dblclick", function(){
+            //클릭한 div의 인덱스를 localstorage에 저장
+            localStorage.setItem("clickedDivNum", index);
+            //이미지 슬라이드 페이지로 이동 
+            window.location.href = "index-pc-slide.html";
+        });
     });
 });
